ac8ed3bc297cb7aa18815a7e30a6066a6617bac7
Lmntria <seymur1095@gmail.com>
2022-10-30 11:59:31 +0400

    one by one call

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,30 +26,30 @@ const BaseLink="https://dummyjson.com"
 
 //   return fetchdata
 // }
-
+let count=1
 
 
 const getdata=()=>{
-  let count=1
-  const fetchdata=fetch(`${BaseLink}/users`)
+
+  const fetchdata=fetch(`${BaseLink}/users/${count}`)
   .then((x)=>x.json())
   .then((data)=>{
-    data.users?.map((item)=>{
-      tableBody.innerHTML+=`
+    tableBody.innerHTML+=`
       <tr>
-          <th scope="row" class="list-number"><img width="50" src="${item.image}"></th>
-          <td>${item.firstName} ${item.lastName}</td>
-          <td>${item.email}</td>
-          <td>${item.address.address},${item.address.city}</td>
-          <td>${item.phone}</td>
+          <th scope="row" class="list-number"><img width="50" src="${data.image}"></th>
+          <td>${data.firstName} ${data.lastName}</td>
+          <td>${data.email}</td>
+          <td>${data.address.address},${data.address.city}</td>
+          <td>${data.phone}</td>
           <td class="text-center">
             <i class="las la-edit btn btn-light btn-yellow fs-5" onclick="redacte(this)"></i>
             <i class="las la-trash btn btn-danger" onclick="this.parentElement.parentElement.remove()"></i>
           </td>
       </tr>
       `
-    })
+
   })
+  count++
   return fetchdata
 }
 submit.addEventListener('click',getdata)
